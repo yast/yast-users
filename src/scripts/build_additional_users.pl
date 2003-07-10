9b2418fcd1e3af00bfbf3f73510c050bdcc6d44c
Jiří Suchomel <jsuchome@suse.cz>
2003-07-10 12:54:28 +0000

    - secondary groups for new users saved in /etc/default/useradd - LDAP users and groups management:     password length checks (length defined in LDAP configuration)     saving last used Id to LDAP config object     solved LDAP groups (groupOfUniqueNames cannot be empty) - adapted testsuite - 2.8.6
    svn path=/trunk/users/; revision=10391

--- src/scripts/build_additional_users.pl
+++ src/scripts/build_additional_users.pl
@@ -18,32 +18,15 @@
 #
 
 $dir              = $ARGV[0];
+$ldap		  = $ARGV[1];
 
 $userlist_file    = $dir."/additional_userlist";
 $more_users_file  = $dir."/additional_moreusers";
 $usernames_file   = $dir."/additional_usernames";
+$userdns_file     = $dir."/additional_userdns";
 
 $output_ulist      = $dir."/additional_ulist.ycp";
 $output_more       = $dir."/additional_more.ycp";
-
-# for debugging
-sub print_date {
-
-    my ($message) = @_;
-#    $date = `date +%X`;
-#    print STDERR "$message: $date";
-}
-
-open USERLIST, "< $userlist_file";
-
-$userlist = <USERLIST>;
-@userlist_list = ();
-if (defined $userlist)
-{
-    @userlist_list = split (/,/, $userlist);
-}
-
-close USERLIST;
 
 open MORE_USERS, "< $more_users_file";
 
@@ -56,41 +39,85 @@ if (defined $more_users)
     
 close MORE_USERS;
 
-open USERNAMES, "< $usernames_file";
-
-foreach my $line (<USERNAMES>)
-{
-    chomp $line;
-    $usernames .= $line;
-}
-
-close USERNAMES;
-
+my $usernames = "";
+my @userlist_list = ();
 my @nameslist = ();
 my $first_idx = 0;
 
-while (index ($usernames, "\"") > -1 && $first_idx > -1)
-{
-    $first_idx = index($usernames, ":");
-    $last_idx = index($usernames, "]");
-    $names = substr ($usernames, $first_idx, $last_idx - $first_idx + 1, "");
-    if (index ($names, "\"") > -1)
+open USERLIST, "< $userlist_file";
+
+# ---------- LDAP uses DN of users, while other types use usernames
+if (defined $ldap) {
+    foreach my $line (<USERLIST>)
     {
-        $names =~s/[:\[\]\"]//g;
-        push (@nameslist, split (/,/, $names));
+        if (index ($line, "\"") > -1) {
+	    $line =~s/[\" \n]//g;
+	    if (substr ($line, -1, 1) eq ",") {
+		chop $line;
+	    }
+	    push (@userlist_list, $line);
+	}
+    }
+    open USERDNS, "< $userdns_file";
+    foreach my $line (<USERDNS>)
+    {
+        if (index ($line, "\"") > -1) {
+	    $line =~s/[\" \n]//g;
+	    if (substr ($line, -1, 1) eq ",") {
+		chop $line;
+	    }
+	    push (@nameslist, $line);
+	}
+    }
+    close USERDNS;
+}
+else {
+    $userlist = <USERLIST>;
+    if (defined $userlist) {
+	@userlist_list = split (/,/, $userlist);
+    }
+
+    # ---- all names of users (could be added to current group)
+    # ---- USERNAMES can contain YCP map of users
+    open USERNAMES, "< $usernames_file";
+    foreach my $line (<USERNAMES>)
+    {
+	chomp $line;
+        $usernames .= $line;
+    }
+    close USERNAMES;
+
+    # ----- generate list of user names
+    while (index ($usernames, "\"") > -1 && $first_idx > -1)
+    {
+	$first_idx = index($usernames, ":");
+        $last_idx = index($usernames, "]");
+	$names = substr ($usernames, $first_idx, $last_idx - $first_idx + 1,"");
+        if (index ($names, "\"") > -1)
+	{
+	    $names =~s/[:\[\]\"]//g;
+            push (@nameslist, split (/,/, $names));
+	}
     }
 }
+close USERLIST;
 
+#--------- now, generate output
 open ULIST, "> $output_ulist";
 open MORE, "> $output_more";
 print ULIST "[\n";
 print MORE "[\n";
 
 %more_map = ();
+$count = 1;
 foreach $user (@more_list)
 {
-    $more_map{$user} = 1; 
-    print MORE "\t`item( `id(\"$user\"), \"$user\", true ),\n";
+    $more_map{$user} = 1;
+    # not necessary to print many users (widget is not editable)
+    if ($count lt 42) {
+	print MORE "\t`item( `id(\"$user\"), \"$user\", true ),\n";
+    }
+    $count = $count + 1;
 }
 
 %userlist_map = ();
