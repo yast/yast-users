6878a5c7720a48eaa18782325ecbef0ab2f46d82
Jiří Suchomel <jsuchome@suse.cz>
2009-11-10 10:14:07 +0000

    - YaPI: added ADMINISTRATOR.pm (root password, mail aliases:   fate#307141) - YaPI: enable returning set of attributes (UsersGet, UserGet) - 2.17.28.1
    svn path=/branches/SuSE-Code-11-Branch/users/; revision=59449

---
+++ src/YaPI/ADMINISTRATOR.pm
@@ -0,0 +1,90 @@
+package YaPI::ADMINISTRATOR;
+
+use strict;
+use YaST::YCP qw(:LOGGING);
+use YaPI;
+
+textdomain ("users");
+
+# ------------------- imported modules
+YaST::YCP::Import ("MailAliases");
+YaST::YCP::Import ("Mode");
+YaST::YCP::Import ("Users");
+# -------------------------------------
+
+our $VERSION		= '1.0.0';
+our @CAPABILITIES 	= ('SLES11');
+our %TYPEINFO;
+
+=item *
+C<$hash Read ();>
+
+Returns the information about system administrator (root).
+Currently return hash contains the list of mail aliases.
+
+=cut
+
+BEGIN{$TYPEINFO{Read} = ["function",
+    [ "map", "string", "any" ]];
+}
+sub Read {
+
+    my $self	= shift;
+
+    # FIXME HACK to prevent setting mode to testsuite (bnc#243624)
+    Mode->SetUI ("commandline");
+
+    my $root_mail	= MailAliases->GetRootAlias ();
+    return {} if !defined $root_mail;
+
+    my @root_aliases	= ();
+    foreach my $alias (split (/,/, $root_mail)) {
+	$alias	=~ s/[ \t]//g;
+	push @root_aliases, $alias;
+    }
+
+    return {
+	"aliases"	=> \@root_aliases
+    }
+}
+
+=item *
+C<$string Write ($argument_hash);>
+
+write the system adminstrator data. Supported keys of the argument hash are:
+
+    "aliases"	=> list of mail aliases
+    "password"	=> new password
+    
+Returns error message on error.
+
+=cut
+
+BEGIN{$TYPEINFO{Write} = ["function",
+    "string",
+    [ "map", "string", "any" ]];
+}
+sub Write {
+
+    my $self	= shift;
+    my $args	= shift;
+    my $ret	= "";
+
+    Mode->SetUI ("commandline");
+
+    if ($args->{"aliases"} && ref ($args->{"aliases"}) eq "ARRAY") {
+
+	my $root_mail	= join (", ", @{$args->{"aliases"}});
+	if (!MailAliases->SetRootAlias ($root_mail)) {
+	    # error popup
+	    $ret = __("An error occurred while setting forwarding for root's mail.");
+	    return $ret;
+	}
+    }
+    if ($args->{"password"}) {
+	Users->SetRootPassword ($args->{"password"});
+	Users->WriteRootPassword ();
+    }
+    return $ret;
+}
+
