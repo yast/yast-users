a8a1d94bb9aa71e5110c718431f2e86268584530
Jiří Suchomel <jsuchome@suse.cz>
2011-11-10 11:20:04 +0000

    - do not recrypt group password provided in autoYaST profile   (bnc#722421) - add GID to autoYaST group export map in more cases   (backport of bnc#540787) - 2.17.51
    svn path=/branches/SuSE-Code-11-SP2-Branch/users/; revision=66784

--- src/Users.pm
+++ src/Users.pm
@@ -1636,6 +1636,10 @@ sub Read {
 		undef %user_in_work;
 		$users{$type}{$id}	= \%user;
 	    }
+	    # do not re-crypt group password value (bnc#722421)
+	    foreach my $id (keys %{$groups{$type}}) {
+		$groups{$type}{$id}{"encrypted"}	= YaST::YCP::Boolean (1);
+	    }
 	}
     }
 
@@ -5929,8 +5933,22 @@ sub ImportGroup {
 	    }
 	}
     }
+    my $encrypted	= $group{"encrypted"};
+    $encrypted		= YaST::YCP::Boolean (1) if !defined $encrypted;
+    if (defined $encrypted && ref ($encrypted) ne "YaST::YCP::Boolean") {
+	$encrypted	= YaST::YCP::Boolean ($encrypted);
+    }
+    my $pass		= $group{"group_password"};
+    if ((!defined $encrypted || !bool ($encrypted)) &&
+	(defined $pass) && !Mode->config ())
+    {
+	$pass 		= $self->CryptPassword ($pass, $type);
+	$encrypted	= YaST::YCP::Boolean (1);
+    }
+
     my %ret		= (
-        "userPassword"	=> $group{"group_password"},
+	"userPassword"	=> $pass,
+	"encrypted"	=> $encrypted,
         "cn"		=> $groupname,
         "gidNumber"	=> $gid,
         "userlist"	=> \%userlist,
@@ -6383,7 +6401,7 @@ sub ExportGroup {
         "groupname"		=> $group->{"cn"}		|| "",
         "userlist"		=> $userlist
     );
-    if (($group->{"modified"} || "edited") ne "edited" ||
+    if (($group->{"modified"} || "") ne "edited" ||
 	(defined $group->{"org_group"} &&
 	(defined $group->{"org_group"}{"gidNumber"} &&
 	$group->{"gidNumber"} ne $group->{"org_group"}{"gidNumber"})
@@ -6395,6 +6413,12 @@ sub ExportGroup {
 	$ret{"gid"}		= $group->{"gidNumber"};
     }
     if (defined $group->{"userPassword"}) {
+
+    	my $encrypted	= bool ($group->{"encrypted"});
+    	if (!defined $group->{"encrypted"}) {
+	    $encrypted	= 1;
+	}
+	$ret{"encrypted"}	= YaST::YCP::Boolean ($encrypted);
 	$ret{"group_password"}	= $group->{"userPassword"};
     }
 
