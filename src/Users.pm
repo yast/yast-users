52db08bd639ad4a091cdd500b1aabe50d248b240
Jiří Suchomel <jsuchome@suse.cz>
2012-01-25 13:16:00 +0000

    - confirmed license - 2.22.0
    svn path=/trunk/users/; revision=67293

--- src/Users.pm
+++ src/Users.pm
@@ -1,4 +1,24 @@
 #! /usr/bin/perl -w
+# ------------------------------------------------------------------------------
+# Copyright (c) 2006-2012 Novell, Inc. All Rights Reserved.
+#
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of version 2 of the GNU General Public License as published by the
+# Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, contact Novell, Inc.
+#
+# To contact Novell about this file by physical or electronic mail, you may find
+# current contact information at www.novell.com.
+# ------------------------------------------------------------------------------
+#
+
 #
 # File:			modules/Users.pm
 # Package:		Configuration of users and groups
@@ -1258,7 +1278,7 @@ sub ReadSourcesSettings {
 
     $nis_available		= ReadNISAvailable ();
     $nis_master 		= ReadNISMaster ();
-    $ldap_available 		= UsersLDAP->ReadAvailable ();
+    $ldap_available 		= 1;#UsersLDAP->ReadAvailable ();
 
     if (!$nis_master && $nis_available) {
         push @available_usersets, "nis";
