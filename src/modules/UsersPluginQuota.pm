yast/yast-users

commit fe305b9fff711bb9b658443506b7f97e05f8065b

hardening shell invocation

src/modules/UsersPluginQuota.pm
@@ -42,6 +42,7 @@ our %TYPEINFO;
 ##--------------------- global imports
 
 YaST::YCP::Import ("SCR");
+YaST::YCP::Import ("String");
 
 ##--------------------------------------
 ##--------------------- global variables
@@ -105,7 +106,7 @@ sub remove_plugin_data {
 sub get_quota_enabled_filesystems {
 
     if (! @quota_enabled_filesystems) {
-	my $cmd	= "LANG=C grep quota /etc/mtab | cut -f 1 -d ' '";
+	my $cmd	= "LANG=C /usr/bin/grep quota /etc/mtab | /usr/bin/cut -f 1 -d ' '";
 	my $out	= SCR->Execute (".target.bash_output", $cmd);
 	if ($out->{"stdout"}) {
 	    # each line in stdout reports quota for one filesystem
@@ -128,14 +129,14 @@ sub read_quota_info {
 
 	my $opt	= "-u ".$data->{"uid"} if defined $data->{"uid"};
 	if ($config->{"what"} eq "group") {
-	    $opt	= "-g ".$data->{"cn"} if defined $data->{"cn"};
+	    $opt	= "-g '".String->Quote($data->{"cn"})."'" if defined $data->{"cn"};
 	}
 	return $data if not defined $opt;
 
 	my %fsystems	= ();
 	my @quotalist	= ();
 
-	my $cmd	= "LANG=C quota $opt -pv 2>/dev/null | tail -n +3";
+	my $cmd	= "LANG=C /usr/bin/quota $opt -pv 2>/dev/null | /usr/bin/tail -n +3";
 	my $out	= SCR->Execute (".target.bash_output", $cmd);
 	if ($out->{"stdout"}) {
 	    # each line in stdout reports quota for one filesystem
@@ -186,7 +187,8 @@ sub is_quota_available {
 sub has_quota {
 
     my $opt = shift;
-    my $out = SCR->Execute (".target.bash_output", "LANG=C quota $opt -v 2>/dev/null | tail -n +3");
+    # cannot escape opt here as it is caller responsibility
+    my $out = SCR->Execute (".target.bash_output", "LANG=C /usr/bin/quota $opt -v 2>/dev/null | /usr/bin/tail -n +3");
     return ($out->{"stdout"});
 }
 
@@ -401,22 +403,22 @@ sub Write {
     # do nothing for user intended for deletion
     return YaST::YCP::Boolean (1) if ($config->{"modified"} || "") eq "deleted";
 
-    my $opt	= "-u ".$data->{"uid"} if defined $data->{"uid"};
+    my $opt	= "-u '".String->Quote($data->{"uid"})"'" if defined $data->{"uid"};
     if ($config->{"what"} eq "group") {
-	$opt	= "-g ".$data->{"cn"};
+	$opt	= "-g '".String->Quote($data->{"cn"})."'";
     }
     return YaST::YCP::Boolean (1) if not $opt;
-    
+
     foreach my $qmap (@{$data->{"quota"}}) {
-	my $quota_blocks_soft	= $qmap->{"quota_blocks_soft"} || 0;    
-	my $quota_blocks_hard	= $qmap->{"quota_blocks_hard"} || 0;    
-	my $quota_inodes_soft	= $qmap->{"quota_inodes_soft"} || 0;    
-	my $quota_inodes_hard	= $qmap->{"quota_inodes_hard"} || 0;    
+	my $quota_blocks_soft	= $qmap->{"quota_blocks_soft"} || 0;
+	my $quota_blocks_hard	= $qmap->{"quota_blocks_hard"} || 0;
+	my $quota_inodes_soft	= $qmap->{"quota_inodes_soft"} || 0;
+	my $quota_inodes_hard	= $qmap->{"quota_inodes_hard"} || 0;
 	my $quota_blocks_grace	= $qmap->{"quota_blocks_grace"} || 0;
 	my $quota_inodes_grace	= $qmap->{"quota_inodes_grace"} || 0;
 	my $quota_fs		= $qmap->{"quota_fs"};
 	next if not $quota_fs;
-	my $cmd	= "setquota $opt $quota_blocks_soft $quota_blocks_hard $quota_inodes_soft $quota_inodes_hard $quota_fs";
+	my $cmd	= "/usr/sbin/setquota $opt $quota_blocks_soft $quota_blocks_hard $quota_inodes_soft $quota_inodes_hard '".String->Quote($quota_fs)."'";
 	my $out	= SCR->Execute (".target.bash_output", $cmd);
 	if ($out->{"exit"} && $out->{"stderr"}) {
 	    y2error ("error calling $cmd: ", $out->{"stderr"});
@@ -427,7 +429,7 @@ sub Write {
 	    return YaST::YCP::Boolean (0);
 	}
 	if ($quota_blocks_grace > 0 || $quota_inodes_grace > 0) {
-	    $cmd	= "setquota -T $opt $quota_blocks_grace $quota_inodes_grace $quota_fs";
+	    $cmd	= "/usr/sbin/setquota -T $opt $quota_blocks_grace $quota_inodes_grace '".String->Quote($quota_fs)."'";
 	    $out	= SCR->Execute (".target.bash_output", $cmd);
 	    if ($out->{"exit"} && $out->{"stderr"}) {
 		y2error ("error calling $cmd: ", $out->{"stderr"});

src/clients/users_plugin_ldap_shadowaccount.rb
@@ -32,7 +32,9 @@
 # Authors:
 #	Jiri Suchomel <jsuchome@suse.cz>
 #
-# $Id$
+
+require "shellwords"
+
 module Yast
   class UsersPluginLdapShadowaccountClient < Client
     def main
@@ -109,8 +111,8 @@ module Yast
             SCR.Execute(
               path(".target.bash_output"),
               Builtins.sformat(
-                "date --date='1970-01-01 00:00:01 %1 days' +\"%%x\"",
-                @last_change
+                "/usr/bin/date --date='1970-01-01 00:00:01 %1 days' +\"%%x\"",
+                @last_change.to_s.shellescape
               )
             )
           )
@@ -121,17 +123,9 @@ module Yast
           @last_change = _("Never")
         end
         if @expires != "0" && @expires != "-1" && @expires != ""
-          @out = Convert.to_map(
-            SCR.Execute(
-              path(".target.bash_output"),
-              Ops.add(
-                Builtins.sformat(
-                  "date --date='1970-01-01 00:00:01 %1 days' ",
-                  @expires
-                ),
-                "+\"%Y-%m-%d\""
-              )
-            )
+          @out = SCR.Execute(
+            path(".target.bash_output"),
+            "/usr/bin/date --date='1970-01-01 00:00:01 %1 days' #{@expires.to_s.shellescape}+\"%Y-%m-%d\""
           )
           # remove \n from the end
           @exp_date = Builtins.deletechars(
@@ -258,14 +252,9 @@ module Yast
               if @exp_date == ""
                 Ops.set(@tmp_data, "shadowExpire", "")
               else
-                @out = Convert.to_map(
-                  SCR.Execute(
-                    path(".target.bash_output"),
-                    Ops.add(
-                      Builtins.sformat("date --date='%1 UTC' ", @exp_date),
-                      "+%s"
-                    )
-                  )
+                @out = SCR.Execute(
+                  path(".target.bash_output"),
+                  "/usr/bin/date --date='%1 UTC' #{@exp_date.to_s.shellescape}+%s"
                 )
                 @seconds_s = Builtins.deletechars(
                   Ops.get_string(@out, "stdout", "0"),

src/include/users/cmdline.rb
@@ -390,7 +390,7 @@ module Yast
       out = Convert.to_map(
         SCR.Execute(
           path(".target.bash_output"),
-          Builtins.sformat("ps --no-headers -u %1", uid)
+          Builtins.sformat("/usr/bin/ps --no-headers -u %1", uid.to_i)
         )
       )
       if Builtins.size(Ops.get_string(out, "stdout", "")) != 0

src/include/users/dialogs.rb
@@ -27,6 +27,8 @@
 #
 # $Id$
 
+require "shellwords"
+
 require "users/ssh_public_key"
 require "yast2/popup"
 
@@ -1576,14 +1578,9 @@ module Yast
             if exp_date == ""
               user["shadowExpire"] = ""
             else
-              out = Convert.to_map(
-                SCR.Execute(
-                  path(".target.bash_output"),
-                  Ops.add(
-                    Builtins.sformat("date --date='%1 UTC' ", exp_date),
-                    "+%s"
-                  )
-                )
+              out = SCR.Execute(
+                path(".target.bash_output"),
+                "/usr/bin/date --date=#{exp_date.shellescape}%1' UTC' +%s"
               )
               seconds_s = Builtins.deletechars(
                 Ops.get_string(out, "stdout", "0"),

src/include/users/routines.rb
@@ -25,7 +25,9 @@
 # Authors:	Johannes Buchhold <jbuch@suse.de>,
 #          Jiri Suchomel <jsuchome@suse.cz>
 #
-# $Id$
+
+require "shellwords"
+
 module Yast
   module UsersRoutinesInclude
     def initialize_users_routines(include_target)
@@ -95,14 +97,12 @@ module Yast
 
     # if the user has log on system
     def UserLogged(name)
-      out = Convert.to_map(
-        SCR.Execute(
-          path(".target.bash_output"),
-          Builtins.sformat("ps --no-headers -u %1", name)
-        )
+      out = SCR.Execute(
+        path(".target.bash_output"),
+        "/usr/bin/ps --no-headers -u #{name.shellescape}"
       )
-      proc = Ops.get_string(out, "stdout", "")
-      Builtins.size(proc) != 0 && !Mode.config
+      output = Ops.get_string(out, "stdout", "")
+      Builtins.size(output) != 0 && !Mode.config
     end
   end
 

src/include/users/widgets.rb
@@ -24,7 +24,9 @@
 # Summary:	Widgets definitions and helper functions
 # Authors:	Jiri Suchomel <jsuchome@suse.cz>
 #
-# $Id$
+
+require "shellwords"
+
 module Yast
   module UsersWidgetsInclude
     def initialize_users_widgets(include_target)
@@ -1830,17 +1832,9 @@ Continue anyway?"))
       exp_date = ""
       if expire != "0" && expire != ""
         # 'expire' is expected to be number of days since 1970-01-01
-        out = Convert.to_map(
-          SCR.Execute(
-            path(".target.bash_output"),
-            Ops.add(
-              Builtins.sformat(
-                "date --date='1970-01-01 00:00:01 %1 days' ",
-                expire
-              ),
-              "+\"%Y-%m-%d\""
-            )
-          )
+        out = SCR.Execute(
+          path(".target.bash_output"),
+          "/usr/bin/date --date='1970-01-01 00:00:01 '#{expire.shellescape}' days' +\"%Y-%m-%d\""
         )
         exp_date = Builtins.deletechars(Ops.get_string(out, "stdout", ""), "\n")
       end
@@ -1881,14 +1875,9 @@ Continue anyway?"))
       if new_exp_date == ""
         new_expire = ""
       else
-        out = Convert.to_map(
-          SCR.Execute(
-            path(".target.bash_output"),
-            Ops.add(
-              Builtins.sformat("date --date='%1 UTC' ", new_exp_date),
-              "+%s"
-            )
-          )
+        out = SCR.Execute(
+          path(".target.bash_output"),
+          "/usr/bin/date --date=#{new_exp_date.shellescape}' UTC' +%s"
         )
         seconds_s = Builtins.deletechars(
           Ops.get_string(out, "stdout", "0"),

src/lib/users/ssh_authorized_keys_file.rb
@@ -16,6 +16,8 @@
 #  To contact SUSE about this file by physical or electronic mail,
 #  you may find current contact information at www.suse.com
 
+require "shellwords"
+
 Yast.import "FileUtils"
 
 module Yast
@@ -113,7 +115,7 @@ module Yast
         if FileUtils::Exists(path)
           raise NotRegularFile unless FileUtils::IsFile(path)
         else
-          SCR.Execute(Path.new(".target.bash"), "umask 0077 && touch #{path}")
+          SCR.Execute(Path.new(".target.bash"), "umask 0077 && /usr/bin/touch #{path.shellescape}")
         end
         content = keys.join("\n") + "\n"
         SCR.Write(Path.new(".target.string"), path, content)

src/modules/Users.pm
@@ -289,7 +289,7 @@ BEGIN { $TYPEINFO{LastChangeIsNow} = ["function", "string"]; }
 sub LastChangeIsNow {
     if (Mode->test ()) { return "0";}
 
-    my %out = %{SCR->Execute (".target.bash_output", "date +%s")};
+    my %out = %{SCR->Execute (".target.bash_output", "/usr/bin/date +%s")};
     my $seconds = $out{"stdout"} || "0";
     chomp $seconds;
     return sprintf ("%u", $seconds / (60*60*24));
@@ -3963,7 +3963,7 @@ sub WriteSecurity {
     my $self	= shift;
     my $ret = 1;
     if ($security_modified) {
-	
+
 	my %security	= (
 	    "PASSWD_ENCRYPTION"	=> $encryption_method
 	);
@@ -3982,7 +3982,7 @@ sub WriteSecurity {
 BEGIN { $TYPEINFO{WriteGroup} = ["function", "boolean"]; }
 sub WriteGroup {
 
-    my $cmd	= "/bin/cp $base_directory/group $base_directory/group.YaST2save";
+    my $cmd	= "/usr/bin/cp '".String->Quote($base_directory)."/group' '".String->Quote($base_directory)."/group.YaST2save'";
     if (SCR->Execute (".target.bash", $cmd) != 0)
     {
 	y2error ("creating backup of $base_directory/group failed");
@@ -3990,7 +3990,7 @@ sub WriteGroup {
     }
     y2usernote ("Backup created: '$cmd'");
     my $ret	= UsersPasswd->WriteGroups (\%groups);
-    $cmd	= "diff -U 1 $base_directory/group.YaST2save $base_directory/group";
+    $cmd	= "/usr/bin/diff -U 1 '".String->Quote($base_directory)."/group.YaST2save' '".String->Quote($base_directory)."/group'";
     my $out	= SCR->Execute (".target.bash_output", $cmd);
     my $stdout	= $out->{"stdout"} || "";
     y2usernote ("Comparing original and new version:
@@ -4001,7 +4001,7 @@ $stdout`");
 ##------------------------------------
 BEGIN { $TYPEINFO{WritePasswd} = ["function", "boolean"]; }
 sub WritePasswd {
-    my $cmd	= "/bin/cp $base_directory/passwd $base_directory/passwd.YaST2save";
+    my $cmd	= "/usr/bin/cp '".String->Quote($base_directory)."/passwd' '".String->Quote($base_directory)."/passwd.YaST2save'";
     if (SCR->Execute (".target.bash", $cmd) != 0)
     {
 	y2error ("creating backup of $base_directory/passwd failed");
@@ -4009,7 +4009,7 @@ sub WritePasswd {
     }
     y2usernote ("Backup created: '$cmd'");
     my $ret	= UsersPasswd->WriteUsers (\%users);
-    $cmd	= "diff -U 1 $base_directory/passwd.YaST2save $base_directory/passwd";
+    $cmd	= "/usr/bin/diff -U 1 '".String->Quote($base_directory)."/passwd.YaST2save' '".String->Quote($base_directory)."/passwd'";
     my $out	= SCR->Execute (".target.bash_output", $cmd);
     my $stdout	= $out->{"stdout"} || "";
     y2usernote ("Comparing original and new version:
@@ -4020,8 +4020,8 @@ $stdout`");
 ##------------------------------------
 BEGIN { $TYPEINFO{WriteShadow} = ["function", "boolean"]; }
 sub WriteShadow {
-    
-    my $cmd	= "/bin/cp $base_directory/shadow $base_directory/shadow.YaST2save";
+
+    my $cmd	= "/usr/bin/cp '".String->Quote($base_directory)."/shadow' '".String->Quote($base_directory)."/shadow.YaST2save'";
     if (SCR->Execute (".target.bash", $cmd) != 0)
     {
 	if (FileUtils->Exists ("$base_directory/shadow")) {
@@ -4064,19 +4064,19 @@ sub WriteAuthorizedKeys {
 sub PreDeleteUsers {
 
     my $ret = 1;
-    
+
     if ($userdel_precmd eq "" || !FileUtils->Exists ($userdel_precmd)) {
 	return $ret;
     }
-    
+
     foreach my $type ("system", "local") {
 	if (!defined $removed_users{$type}) {
 	    next;
 	}
 	foreach my $username (keys %{$removed_users{$type}}) {
 	    my %user = %{$removed_users{$type}{$username}};
-	    my $cmd = sprintf ("$userdel_precmd $username %i %i %s",
-		$user{"uidNumber"}, $user{"gidNumber"}, $user{"homeDirectory"});
+	    my $cmd = sprintf ("$userdel_precmd '".String->Quote($username)."' %i %i '%s'",
+		$user{"uidNumber"}, $user{"gidNumber"}, String->Quote($user{"homeDirectory"}));
 	    SCR->Execute (".target.bash", $cmd);
 	    y2usernote ("User pre-deletion script called: '$cmd'");
 	};
@@ -4120,8 +4120,8 @@ sub PostDeleteUsers {
 	    my %user = %{$removed_users{$type}{$username}};
 	    my $uid     = $user{"uidNumber"} || 0;
 	    Syslog->Log ("User deleted by YaST: name=$username, UID=$uid");
-	    my $cmd = sprintf ("$userdel_postcmd $username $uid %i %s",
-              $user{"gidNumber"} || 0, $user{"homeDirectory"} || "");
+	    my $cmd = sprintf ("$userdel_postcmd '".String->Quote($username)."' $uid %i '%s'",
+              $user{"gidNumber"} || 0, String->Quote($user{"homeDirectory"} || ""));
 	    SCR->Execute (".target.bash", $cmd);
 	    # call the "Write" function from plugins...
 	    my $args	= {
@@ -5029,9 +5029,9 @@ sub IsDirWritable {
         $tmpfile .= "0";
     }
 
-    my %out = %{SCR->Execute (".target.bash_output", "/bin/touch $tmpfile")};
+    my %out = %{SCR->Execute (".target.bash_output", "/usr/bin/touch '".String->Quote($tmpfile)."'")};
     if (defined $out{"exit"} && $out{"exit"} == 0) {
-        SCR->Execute (".target.bash", "/bin/rm $tmpfile");
+        SCR->Execute (".target.bash", "/usr/bin/rm '".String->Quote($tmpfile)."'");
         return "";
     }
     return $dir;
@@ -5340,7 +5340,7 @@ sub CheckGroupname {
         return __("No group name entered.
 Try again.");
     }
-    
+
     my $min	= UsersCache->GetMinGroupnameLength ();
     my $max	= UsersCache->GetMaxGroupnameLength ();
 
@@ -5357,10 +5357,10 @@ Try again.");
 	return sprintf (__("The group name must be between %i and %i characters in length.
 Try again."), $min, $max);
     }
-	
+
     my $filtered = $groupname;
 
-    my $grep = SCR->Execute (".target.bash_output", "echo '$filtered' | grep '\^$character_class\$'", { "LANG" => "C" });
+    my $grep = SCR->Execute (".target.bash_output", "/usr/bin/echo '".String->Quote($filtered)."' | /usr/bin/grep '\^".String->Quote($character_class)."\$'", { "LANG" => "C" });
 
     my $stdout = $grep->{"stdout"} || "";
     $stdout =~ s/\n//g;
@@ -5372,7 +5372,7 @@ letters, digits, \"-\", \".\", and \"_\"
 and must begin with a letter.
 Try again.");
     }
-    
+
     if ($groupname_changed && UsersCache->GroupnameExists ($groupname)) {
 	# error popup
 	return __("There is a conflict between the entered

commit 16bd272093a04550fdfdd99fa3351d5b3aa01eee

Fix a syntax error
introduced in fe305b9fff711bb9b658443506b7f97e05f8065b

src/modules/UsersPluginQuota.pm
@@ -403,7 +403,7 @@ sub Write {
     # do nothing for user intended for deletion
     return YaST::YCP::Boolean (1) if ($config->{"modified"} || "") eq "deleted";
 
-    my $opt	= "-u '".String->Quote($data->{"uid"})"'" if defined $data->{"uid"};
+    my $opt	= "-u '".String->Quote($data->{"uid"})."'" if defined $data->{"uid"};
     if ($config->{"what"} eq "group") {
 	$opt	= "-g '".String->Quote($data->{"cn"})."'";
     }