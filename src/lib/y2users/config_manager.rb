574ff0ebd8080e9e36518a67803843a04948c748
Josef Reidinger <jreidinger@suse.cz>
2021-04-30 21:53:04 +0200

    adapt API of config manager due to review

--- src/lib/y2users/config_manager.rb
+++ src/lib/y2users/config_manager.rb
@@ -17,32 +17,66 @@
 # To contact SUSE LLC about this file by physical or electronic mail, you may
 # find current contact information at www.suse.com.
 
+require "singleton"
+require "y2users/config"
+
 module Y2Users
   # Holds references to different configuration instances.
   # It is not mandatory for config to be registered here.
   class ConfigManager
-    class << self
-      # @return [Hash<Symbol, Config> register of hold configs.
-      def configs
-        @configs ||= {}
+    include Singleton
+
+    def initialize
+      @register = {}
+    end
+
+    # Register config as given id
+    # @param [Config] config
+    # @param [Symbol] as id of config
+    # @note if given id is already registered, it is overwritten
+    def register(config, as:)
+      @register[as] = config
+    end
+
+    # Unregister given id
+    # @param [Symbol] id of config
+    def unregister(id)
+      @register.delete(id)
+    end
+
+    # List of registered ids
+    # @return [Array<Symbol>]
+    def known_ids
+      @register.keys
+    end
+
+    # returns config for given id
+    # @param [Symbol] id of config
+    # @return [Config,nil] config registered for given id or nil if not found
+    def config(id)
+      @register[id]
+    end
+
+    # Gets system configuration.
+    # @param [#read_to(config)] reader used to read system configuration.
+    #   If not specified it will decide itself to use.
+    # @param [Boolean] force_read if it can get previous result of system or
+    #   force re-read from system.
+    # @return [Config]
+    def system(reader: nil, force_read: false)
+      res = config(:system)
+      return res if res && !force_read
+
+      if !reader
+        require "y2users/linux/reader"
+        reader = Linux::Reader.new
       end
 
-      def system(reader: nil, force_read: false)
-        res = configs[:system]
-        return res if res && !force_read
+      res = Config.new
+      reader.read_to(res)
+      register(res, as: :system)
 
-        if !reader
-          require "y2users/linux/reader"
-          reader = Linux::Reader.new
-        end
-
-        require "y2users/config"
-        res = Config.new
-        reader.read_to(res)
-        configs[:system] = res
-
-        res
-      end
+      res
     end
   end
 end
