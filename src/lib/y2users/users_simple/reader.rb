9fb0e5a399feef5b61b44329cb9b8fcf97475301
David Díaz González <dgonzalez@suse.de>
2021-05-04 15:00:01 +0100

    More UsersSimple::Reader changes

--- src/lib/y2users/users_simple/reader.rb
+++ src/lib/y2users/users_simple/reader.rb
@@ -17,28 +17,24 @@
 # To contact SUSE LLC about this file by physical or electronic mail, you may
 # find current contact information at www.suse.com.
 
-require "yast2/execute"
 require "date"
-
-require "y2users/group"
+require "yast2/execute"
 require "y2users/user"
+require "y2users/group"
 require "y2users/password"
 
 Yast.import "UsersSimple"
 
 module Y2Users
   module UsersSimple
-    # Class for reading users configuration from old Yast Module UsersSimple.
+    # Class for reading users configuration from old {Yast::UsersSimple} module
     class Reader
+      # Attaches users to given configuration
+      #
+      # @param config [Config]
       def read_to(config)
-        users = Yast::UsersSimple.GetUsers
-        # TODO: only created users, not imported ones for now
-        users.each do |user_attrs|
-          user = User.new(user_attrs["uid"])
-          user.gecos = [user_attrs["cn"]]
-          user.password = Password.create_plain(user_attrs["userPassword"])
-          config.attach(user)
-        end
+        # Read new users
+        config.attach(users)
 
         # Read also root user settings
         config.attach(root_user)
@@ -46,6 +42,23 @@ module Y2Users
 
     private
 
+      # Returns the list users
+      #
+      # TODO: only created users, not imported ones for now
+      #
+      # @returns [Array<User>] the collection of users
+      def users
+        Yast::UsersSimple.GetUsers.map do |user_attrs|
+          user = User.new(user_attrs["uid"])
+          user.gecos = [user_attrs["cn"]]
+          user.password = Password.create_plain(user_attrs["userPassword"])
+          user
+        end
+      end
+
+      # Returns the root user
+      #
+      # @returns [User] the root user
       def root_user
         user = User.new("root")
         user.gecos = ["root"]
