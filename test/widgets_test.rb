46efbe96d1d752e2ae6149833f35402f726501b7
David Díaz González <dgonzalez@suse.de>
2021-05-06 14:13:35 +0100

    Adapt unit tests

--- test/widgets_test.rb
+++ test/widgets_test.rb
@@ -8,6 +8,16 @@ def stub_widget_value(id, value)
 end
 
 describe Users::PasswordWidget do
+  let(:root_user) { Y2Users::User.new("root") }
+  let(:password) { Y2Users::Password.create_plain(pwd) }
+  let(:pwd) { "s3cr3T" }
+
+  before do
+    allow(root_user).to receive(:root?).and_return(true)
+    allow(subject).to receive(:root_user).and_return(root_user)
+    allow(Y2Users::Password).to receive(:create_plain).and_return(password)
+  end
+
   it "has help text" do
     expect(subject.help).to_not be_empty
   end
@@ -17,9 +27,13 @@ describe Users::PasswordWidget do
   end
 
   context "initialization" do
+    let(:pwd) { "paranoic" }
+
+    before do
+      allow(root_user).to receive(:password).and_return(password)
+    end
+
     it "initializes password to current value" do
-      pwd = "paranoic"
-      allow(Yast::UsersSimple).to receive(:GetRootPassword).and_return(pwd)
       expect(Yast::UI).to receive(:ChangeWidget).with(Id(:pw1), :Value, pwd)
       expect(Yast::UI).to receive(:ChangeWidget).with(Id(:pw2), :Value, pwd)
 
@@ -59,24 +73,28 @@ describe Users::PasswordWidget do
       expect(subject.validate).to eq false
     end
 
-    it "reports error if password contain forbidden characters" do
+    it "reports error if password does not validate" do
       stub_widget_value(:pw1, "mimic_forbidden")
       stub_widget_value(:pw2, "mimic_forbidden")
 
-      expect(Yast::UsersSimple).to receive(:CheckPassword).with("mimic_forbidden", "local")
-        .and_return("Invalid password")
+      fatal_issue = Y2Issues::Issue.new("Invalid password", severity: :fatal)
+      issues = Y2Issues::List.new([fatal_issue])
+
+      allow(root_user).to receive(:password_issues).and_return(issues)
+
       expect(Yast::Report).to receive(:Error)
       expect(Yast::UI).to receive(:SetFocus).with(Id(:pw1))
 
       expect(subject.validate).to eq false
     end
 
-    it "asks for confirmation if password is not strong enough" do
+    it "asks for confirmation if password validates with warnings" do
       stub_widget_value(:pw1, "a")
       stub_widget_value(:pw2, "a")
 
-      allow(Yast::UsersSimple).to receive(:CheckPassword).and_return("")
-      allow(Users::LocalPassword).to receive(:new).and_return(double(valid?: false, errors: ["E"]))
+      warning_issue = Y2Issues::Issue.new("Not strong password")
+      issues = Y2Issues::List.new([warning_issue])
+      allow(root_user).to receive(:password_issues).and_return(issues)
 
       expect(Yast::UI).to receive(:SetFocus).with(Id(:pw1))
       expect(Yast::Popup).to receive(:YesNo).and_return(false)
@@ -88,8 +106,9 @@ describe Users::PasswordWidget do
       stub_widget_value(:pw1, "a")
       stub_widget_value(:pw2, "a")
 
-      allow(Yast::UsersSimple).to receive(:CheckPassword).and_return("")
-      allow(Users::LocalPassword).to receive(:new).and_return(double(valid?: false, errors: ["E"]))
+      warning_issue = Y2Issues::Issue.new("Not strong password")
+      issues = Y2Issues::List.new([warning_issue])
+      allow(root_user).to receive(:password_issues).and_return(issues)
 
       expect(Yast::UI).to receive(:SetFocus).with(Id(:pw1))
       expect(Yast::Popup).to receive(:YesNo).and_return(true).once
@@ -102,8 +121,8 @@ describe Users::PasswordWidget do
       stub_widget_value(:pw1, "a")
       stub_widget_value(:pw2, "a")
 
-      allow(Yast::UsersSimple).to receive(:CheckPassword).and_return("")
-      allow(Users::LocalPassword).to receive(:new).and_return(double(valid?: true))
+      issues = Y2Issues::List.new
+      allow(root_user).to receive(:password_issues).and_return(issues)
 
       expect(subject.validate).to eq true
     end
@@ -112,13 +131,22 @@ describe Users::PasswordWidget do
   it "stores its value" do
     stub_widget_value(:pw1, "new cool password")
 
-    expect(Yast::UsersSimple).to receive(:SetRootPassword).with("new cool password")
+    expect(Y2Users::Password).to receive(:create_plain).with("new cool password")
+    expect(root_user).to receive(:password=).with(password)
 
     subject.store
   end
 
   context "when the widget is allowed to be empty" do
     subject { described_class.new(allow_empty: true) }
+
+    let(:root_user) { double(Y2Users::User) }
+    let(:password) { double(Y2Users::Password) }
+
+    before do
+      allow(subject).to receive(:root_user).and_return(root_user)
+      allow(Y2Users::Password).to receive(:create_plain).and_return(password)
+    end
 
     it "does not validate the password" do
       stub_widget_value(:pw1, "")
@@ -131,7 +159,7 @@ describe Users::PasswordWidget do
       stub_widget_value(:pw1, "")
       stub_widget_value(:pw2, "")
 
-      expect(Yast::UsersSimple).to_not receive(:SetRootPassword)
+      expect(Y2Users::Password).to_not receive(:create_plain)
       subject.store
     end
 
@@ -139,7 +167,8 @@ describe Users::PasswordWidget do
       stub_widget_value(:pw1, "new cool password")
       stub_widget_value(:pw2, "new cool password")
 
-      expect(Yast::UsersSimple).to receive(:SetRootPassword).with("new cool password")
+      expect(Y2Users::Password).to receive(:create_plain).with("new cool password")
+      expect(root_user).to receive(:password=).with(password)
       subject.store
     end
   end
