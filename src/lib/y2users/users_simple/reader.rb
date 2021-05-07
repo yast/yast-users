0c3cae44cff288af152df4d5c8cd3c9195db2f46
Ancor Gonzalez Sosa <ancor@suse.de>
2021-05-07 15:51:27 +0200

    Improve UsersSimple::Reader

--- src/lib/y2users/users_simple/reader.rb
+++ src/lib/y2users/users_simple/reader.rb
@@ -20,6 +20,7 @@
 require "yast"
 require "y2users/user"
 require "y2users/password"
+require "y2users/parsers/shadow"
 
 Yast.import "UsersSimple"
 
@@ -27,6 +28,13 @@ module Y2Users
   module UsersSimple
     # Class for reading users configuration from old Yast::UsersSimple module
     class Reader
+      # @see #{shadow_string}
+      SORTED_SHADOW_ATTRS = [
+        "uid", "userPassword", "shadowLastChange", "shadowMin", "shadowMax",
+        "shadowWarning", "shadowInactive", "shadowExpire", "shadowFlag"
+      ].freeze
+      private_constant :SORTED_SHADOW_ATTRS
+
       # Attaches users to given configuration
       #
       # @param config [Config]
@@ -42,16 +50,27 @@ module Y2Users
 
       # Returns the list users
       #
-      # TODO: only created users, not imported ones for now
-      #
       # @return [Array<User>] the collection of users
       def users
-        Yast::UsersSimple.GetUsers.map do |user_attrs|
-          user = User.new(user_attrs["uid"])
-          user.gecos = [user_attrs["cn"]]
-          user.password = Password.create_plain(user_attrs["userPassword"])
-          user
-        end
+        Yast::UsersSimple.GetUsers.map { |u| user(u) }
+      end
+
+      # Creates a {User} object based on the data structure of an UsersSimple user
+      #
+      # @param user_attrs [Hash] a user representation in the format used by UsersSimple
+      # @return [User]
+      def user(user_attrs)
+        user = User.new(user_attrs["uid"])
+
+        user.uid   = attr_value(:uidNumber, user_attrs)
+        user.gid   = attr_value(:gidNumber, user_attrs)
+        user.shell = attr_value(:loginShell, user_attrs)
+        user.home  = attr_value(:homeDirectory, user_attrs)
+        user.gecos = [attr_value(:cn, user_attrs)].compact
+
+        user.password = create_password(user_attrs)
+
+        user
       end
 
       # Returns the root user
@@ -60,10 +79,45 @@ module Y2Users
       def root_user
         user = User.new("root")
         user.gecos = ["root"]
-        user.uid = 0
+        user.uid = "0"
         passwd_str = Yast::UsersSimple.GetRootPassword
         user.password = Password.create_plain(passwd_str) unless passwd_str.empty?
         user
+      end
+
+      # Value of the given field for the given UsersSimple user
+      #
+      # @param attr [#to_s] name of the attribute
+      # @param user [Hash] a user representation in the format used by UsersSimple
+      # @return [String, nil] nil if the value is missing or empty
+      def attr_value(attr, user)
+        value = user[attr.to_s]
+        return nil if value == ""
+
+        value
+      end
+
+      # Creates a {Password} object based on the data structure of an UsersSimple user
+      #
+      # @param user [Hash] a user representation in the format used by UsersSimple
+      # @return [Password]
+      def create_password(user)
+        parser = Parsers::Shadow.new
+        content = shadow_string(user)
+
+        password = parser.parse(content).values.first
+        password.value = PasswordPlainValue.new(user["userPassword"]) unless user["encrypted"]
+        password
+      end
+
+      # Entry in /etc/shadow describing the password of the given user
+      #
+      # @param user [Hash] a user representation in the format used by UsersSimple
+      # @return [String]
+      def shadow_string(user)
+        SORTED_SHADOW_ATTRS.map do |attr|
+          user[attr] || ""
+        end.join(":")
       end
     end
   end
