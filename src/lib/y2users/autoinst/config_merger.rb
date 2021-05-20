8b4723027a3b9bffb141f5315895150f52097958
Imobach González Sosa <igonzalezsosa@suse.com>
2021-05-20 15:59:11 +0100

    Implement AutoYaST merging for 1st stage

---
+++ src/lib/y2users/autoinst/config_merger.rb
@@ -0,0 +1,98 @@
+# Copyright (c) [2021] SUSE LLC
+#
+# All Rights Reserved.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of version 2 of the GNU General Public License as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, contact SUSE LLC.
+#
+# To contact SUSE LLC about this file by physical or electronic mail, you may
+# find current contact information at www.suse.com.
+
+require "yast"
+require "y2users"
+
+module Y2Users
+  module Autoinst
+    # Helper class to merge users and groups from one config into another config
+    #
+    # When updating a current user/gruop:
+    #
+    # * It keeps the original uid/gid.
+    # * If a property is not defined in the imported user, it falls back to the
+    #   original value or, if missing too, the default value for that property.
+    class ConfigMerger < Y2Users::ConfigMerger
+      # Merges an element into a config
+      #
+      # @param config [Config] This config is modified
+      # @param element [User, Group]
+      def merge_element(config, element)
+        current_element = find_element(config, element)
+
+        new_element = element.clone
+
+        if current_element
+          new_element.assign_internal_id(current_element.id)
+          merge_attributes(current_element, new_element)
+          config.detach(current_element)
+        end
+
+        config.attach(new_element)
+      end
+
+      # Merges one element into another
+      #
+      # @param from [User, Group] Source element
+      # @param to [User, Group] Target element. This element is modified.
+      def merge_attributes(from, to)
+        case from
+        when User
+          merge_users(from, to)
+        when Group
+          merge_groups(from, to)
+        end
+      end
+
+      # Merges two users
+      #
+      # Only non-nil from the `to` user are considered.
+      #
+      # @param [User] Source user
+      # @param [User] Target user. This user is modified.
+      def merge_users(from, to)
+        to.uid = from.uid
+        to.gid = from.gid
+
+        [:name, :shell, :home, :gecos, :source, :password].each do |attr|
+          next unless to.public_send(attr).nil?
+
+          to.public_send("#{attr}=", from.public_send(attr))
+        end
+      end
+
+      # Merges two groups
+      #
+      # Only non-nil from the `to` group are considered.
+      #
+      # @param [Group] Source group
+      # @param [Group] Target group. This group is modified.
+      def merge_groups(from, to)
+        to.gid = from.gid
+
+        [:name, :users, :source].each do |attr|
+          next unless to.public_send(attr).nil?
+
+          to.public_send("#{attr}=", from.public_send(attr))
+        end
+      end
+    end
+  end
+end
---
+++ test/lib/y2users/autoinst/config_merger_test.rb
@@ -0,0 +1,241 @@
+# Copyright (c) [2021] SUSE LLC
+#
+# All Rights Reserved.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of version 2 of the GNU General Public License as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, contact SUSE LLC.
+#
+# To contact SUSE LLC about this file by physical or electronic mail, you may
+# find current contact information at www.suse.com.
+
+require_relative "../test_helper"
+require "y2users"
+require "y2users/autoinst/config_merger"
+
+describe Y2Users::Autoinst::ConfigMerger do
+  subject { described_class.new(lhs, rhs) }
+
+  let(:lhs) { Y2Users::Config.new }
+
+  let(:rhs) { Y2Users::Config.new }
+
+  describe "#merge" do
+    before do
+      lhs.attach(lhs_users)
+      lhs.attach(lhs_groups)
+    end
+
+    let(:lhs_users) { [] }
+
+    let(:lhs_groups) { [] }
+
+    def lhs_user(name)
+      lhs.users.find { |u| u.name == name }
+    end
+
+    def lhs_group(name)
+      lhs.groups.find { |g| g.name == name }
+    end
+
+    context "when the rhs config contains users" do
+      before do
+        rhs.attach(rhs_users)
+      end
+
+      let(:user1) do
+        user = Y2Users::User.new("test1")
+        user.uid = 1000
+        user.gid = 100
+        user.home = "/home/test1"
+        user
+      end
+
+      let(:user2) do
+        user = Y2Users::User.new("test2")
+        user.uid = 1001
+        user.gid = 101
+        user
+      end
+
+      let(:user3) do
+        user = Y2Users::User.new("test3")
+        user.uid = 1003
+        user.gid = 103
+        user.home = "/home/test3"
+        user
+      end
+
+      let(:rhs_users) { [user1, user2] }
+
+      context "and the lhs config does not contain users with the same name" do
+        let(:lhs_users) { [user3] }
+
+        it "adds the new users to the lhs config" do
+          expect(lhs.users.size).to eq(1)
+
+          subject.merge
+
+          expect(lhs.users.map(&:name)).to contain_exactly("test1", "test2", "test3")
+        end
+
+        it "keeps attributes from rhs users" do
+          subject.merge
+
+          expect(lhs_user("test1")).to eq(user1)
+          expect(lhs_user("test2")).to eq(user2)
+        end
+      end
+
+      context "and the lhs config contains users with the same name" do
+        let(:lhs_users) { [lhs_user1, user3] }
+
+        let(:lhs_user1) do
+          user = user1.clone
+          user.uid = 1100
+          user.gid = 110
+          user.home = "/home/lhs_user1"
+          user
+        end
+
+        it "does not add a new user with the same name to the lhs config" do
+          subject.merge
+
+          expect(lhs.users.map(&:name)).to contain_exactly("test1", "test2", "test3")
+        end
+
+        it "updates the lhs user with the data from the corresponding rhs user" do
+          subject.merge
+
+          expect(lhs_user("test1").home).to eq("/home/test1")
+          expect(lhs_user("test1").uid).to eq(1100)
+          expect(lhs_user("test1").gid).to eq(110)
+        end
+
+        it "keeps the lhs user id" do
+          id = lhs_user("test1").id
+          subject.merge
+
+          expect(lhs_user("test1").id).to eq(id)
+        end
+
+        it "keeps the lhs user uid" do
+          subject.merge
+
+          expect(lhs_user("test1").uid).to eq(1100)
+        end
+
+        it "keeps the lhs user gid" do
+          subject.merge
+
+          expect(lhs_user("test1").gid).to eq(110)
+        end
+
+        context "and rhs is missing some attribute" do
+          let(:rhs_users) { [rhs_user1] }
+
+          let(:rhs_user1) do
+            user = Y2Users::User.new("test1")
+            user.uid = 1003
+            user.gid = 103
+            user
+          end
+
+          it "uses the lhs value for the attribute" do
+            subject.merge
+
+            expect(lhs_user("test1").home).to eq("/home/lhs_user1")
+          end
+        end
+      end
+    end
+
+    context "when the rhs config contains groups" do
+      before do
+        rhs.attach(rhs_groups)
+      end
+
+      let(:group1) do
+        group = Y2Users::Group.new("test1")
+        group.gid = 100
+        group.users_name = ["test1", "test2"]
+        group
+      end
+
+      let(:group2) do
+        group = Y2Users::Group.new("test2")
+        group.gid = 101
+        group.users_name = ["test2", "test3"]
+        group
+      end
+
+      let(:group3) do
+        group = Y2Users::Group.new("test3")
+        group.gid = 103
+        group.users_name = ["test3"]
+        group
+      end
+
+      let(:rhs_groups) { [group1, group2] }
+
+      context "and the lhs config does not contain groups with the same name" do
+        let(:lhs_groups) { [group3] }
+
+        it "adds the new groups to the lhs config" do
+          expect(lhs.groups.size).to eq(1)
+
+          subject.merge
+
+          expect(lhs.groups.map(&:name)).to contain_exactly("test1", "test2", "test3")
+        end
+
+        it "keeps attributes from rhs groups" do
+          subject.merge
+
+          expect(lhs_group("test1")).to eq(group1)
+          expect(lhs_group("test2")).to eq(group2)
+        end
+      end
+
+      context "and the lhs config contains groups with the same name" do
+        let(:lhs_groups) { [lhs_group1, group3] }
+
+        let(:lhs_group1) do
+          group = group1.clone
+          group.gid = 110
+          group.users_name = ["test1"]
+          group
+        end
+
+        it "does not add a new group with the same name to the lhs config" do
+          subject.merge
+
+          expect(lhs.groups.map(&:name)).to contain_exactly("test1", "test2", "test3")
+        end
+
+        it "updates the lhs group with the data from the corresponding rhs group except gid" do
+          subject.merge
+
+          expect(lhs_group("test1")).to_not eq(group1)
+          group1.gid = 110
+          expect(lhs_group("test1")).to eq(group1)
+        end
+
+        it "keeps the lhs group id" do
+          id = lhs_group("test1").id
+          subject.merge
+
+          expect(lhs_group("test1").id).to eq(id)
+        end
+      end
+    end
+  end
+end
