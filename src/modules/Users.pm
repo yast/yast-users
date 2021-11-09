1caa4a6a3f807b1531b16361c47fe0214b45eeb2
David Díaz González <dgonzalez@suse.de>
2021-11-09 13:12:03 +0000

    Remove code from importing group_password
    As it is no longer supported by YaST, it makes no sense to still trying
    to import neither, <group_passsord> nor <encrypted> group attributes

--- src/modules/Users.pm
+++ src/modules/Users.pm
@@ -5655,22 +5655,8 @@ sub ImportGroup {
 	    }
 	}
     }
-    my $encrypted	= $group{"encrypted"};
-    $encrypted		= YaST::YCP::Boolean (1) if !defined $encrypted;
-    if (defined $encrypted && ref ($encrypted) ne "YaST::YCP::Boolean") {
-	$encrypted	= YaST::YCP::Boolean ($encrypted);
-    }
-    my $pass		= $group{"group_password"};
-    if ((!defined $encrypted || !bool ($encrypted)) &&
-	(defined $pass) && !Mode->config ())
-    {
-	$pass 		= $self->CryptPassword ($pass, $type);
-	$encrypted	= YaST::YCP::Boolean (1);
-    }
 
     my %ret		= (
-	"userPassword"	=> $pass,
-	"encrypted"	=> $encrypted,
         "cn"		=> $groupname,
         "gidNumber"	=> $gid,
         "userlist"	=> \%userlist,
